350ae0ee8aca20b5974671c366567594f935376c
Devin Pitcher <devin@devinpitcher.com>
2024-05-31 17:39:04 -0700

    Convert alerts

--- app/alerts.server.ts
+++
@@ -1,6 +0,0 @@
-export async function getAlerts(apiKey: string): Promise<Alert[]> {
-  const response = await fetch(`https://az511.com/api/v2/get/event?key=${apiKey}`);
-  const alerts = (await response.json()) as Alert[];
-
-  return alerts.filter((alert) => alert.RoadwayName.toUpperCase() === "SR-347");
-}
