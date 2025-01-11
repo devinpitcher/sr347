726ecda0ac6e7ea4c54aa45010206618aac0b4a2
Devin Pitcher <devin@devinpitcher.com>
2025-01-11 11:21:25 -0700

    Move 404

--- app/root.tsx
+++ app/root.tsx
@@ -1,7 +1,9 @@
-import { Links, Meta, Outlet, Scripts, ScrollRestoration, useLoaderData } from "@remix-run/react";
+import { Link, Links, Meta, Outlet, Scripts, ScrollRestoration, useLoaderData, useRouteLoaderData } from "@remix-run/react";
 import "./styles.pcss";
 import { json, LoaderFunctionArgs } from "@remix-run/cloudflare";
 import { Providers } from "~/utils/context";
+import { useRouteError } from "react-router";
+import { isRouteErrorResponse } from "@remix-run/router/utils";
 
 export const loader = async ({ context }: LoaderFunctionArgs) => {
   return json({
@@ -10,7 +12,7 @@ export const loader = async ({ context }: LoaderFunctionArgs) => {
 };
 
 export function Layout({ children }: { children: React.ReactNode }) {
-  const { appVersion } = useLoaderData<typeof loader>();
+  const data = useRouteLoaderData<typeof loader>("root");
 
   return (
     <html lang="en">
@@ -22,7 +24,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <script defer src="/u.js" data-website-id="5b5ededc-9eda-47cf-bf1b-34e897a21c62" data-domains="sr347.com" data-host-url="https://sr347.com"></script>
       </head>
       <body className="min-h-screen dark:bg-slate-900 dark:text-white">
-        <Providers appVersion={appVersion}>{children}</Providers>
+        <Providers appVersion={data?.appVersion}>{children}</Providers>
         <ScrollRestoration />
         <Scripts />
       </body>
@@ -32,4 +34,39 @@ export function Layout({ children }: { children: React.ReactNode }) {
 
 export default function App() {
   return <Outlet />;
+}
+
+export function ErrorBoundary() {
+  const error = useRouteError();
+
+  if (isRouteErrorResponse(error)) {
+    return (
+      <main className="relative isolate h-screen">
+        <img
+          src="https://images.unsplash.com/photo-1545972154-9bb223aac798?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3050&q=80&exp=8&con=-15&sat=-75"
+          alt=""
+          className="absolute inset-0 -z-10 h-full w-full object-cover object-top"
+        />
+        <div className="mx-auto max-w-7xl px-6 py-32 text-center sm:py-40 lg:px-8">
+          <p className="text-base font-semibold leading-8 text-white">404</p>
+          <h1 className="mt-4 text-3xl font-bold tracking-tight text-white sm:text-5xl">Page not found</h1>
+          <p className="mt-4 text-base text-white/70 sm:mt-6">Sorry, we couldn’t find the page you’re looking for.</p>
+          <div className="mt-10 flex justify-center">
+            <Link to="/" className="text-sm font-semibold leading-7 text-white">
+              <span aria-hidden="true">&larr;</span> Back to home
+            </Link>
+          </div>
+        </div>
+      </main>
+    );
+  } else if (error instanceof Error) {
+    return (
+      <div>
+        <h1>Error</h1>
+        <p>{error.message}</p>
+      </div>
+    );
+  } else {
+    return <h1>Unknown Error</h1>;
+  }
 }
