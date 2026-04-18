6c7e73c2b8363b0f2a61fefd59b4120277f57aa6
Devin Pitcher <devin@devinpitcher.com>
2026-04-18 11:38:21 -0700

    feat: Fetch current alerts

--- src/lib/alerts.ts
+++ src/lib/alerts.ts
@@ -3,6 +3,13 @@ import { env } from "cloudflare:workers";
 import { db } from "~/database/client";
 import { alertsTable } from "~/database/schema";
 import { buildConflictUpdateColumns } from "~/database/utils";
+import { desc, getTableColumns, gte } from "drizzle-orm";
+
+export async function getCurrentAlerts() {
+  const { raw, ...columns } = getTableColumns(alertsTable);
+  const twoHoursAgo = new Date(Date.now() - 2 * 60 * 60 * 1000);
+  return db.select(columns).from(alertsTable).where(gte(alertsTable.lastSeen, twoHoursAgo)).orderBy(desc(alertsTable.alertId));
+}
 
 export async function updateAlerts() {
   const az511 = new AZ511Service(env.AZ511_API_KEY);
