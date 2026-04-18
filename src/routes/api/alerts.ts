1e0c76188ac88daf1f92d9d5e782dc75ca038e49
Devin Pitcher <devin@devinpitcher.com>
2026-04-18 09:58:45 -0700

    feat: Working on normalizing AZ511 data

--- src/routes/api/alerts.ts
+++ src/routes/api/alerts.ts
@@ -1,51 +1,49 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { Redis } from "@upstash/redis/cloudflare";
-import { env, waitUntil } from "cloudflare:workers";
-import { dayjs } from "~/lib/dayjs";
+import { env } from "cloudflare:workers";
+import { AZ511Service } from "~/services/az511";
+import { db } from "~/database/client";
+import { alertsTable } from "~/database/schema";
+import { buildConflictUpdateColumns } from "~/database/utils";
+import { getTableColumns } from "drizzle-orm";
 
 export const Route = createFileRoute("/api/alerts")({
   server: {
     handlers: {
       GET: async () => {
-        const redis = new Redis({
-          url: env.UPSTASH_REDIS_REST_URL,
-          token: env.UPSTASH_REDIS_REST_TOKEN,
+        const { raw, ...columns } = getTableColumns(alertsTable);
+        const alerts = await db.select(columns).from(alertsTable);
+
+        return Response.json({
+          alerts,
+          lastUpdated: Date.now(),
         });
+      },
+      POST: async () => {
+        const az511 = new AZ511Service(env.AZ511_API_KEY);
+        const alerts = await az511.getAlerts();
+        const now = new Date();
 
-        const value = await redis.get<AlertsResponse>("alerts");
+        const operations = alerts.map(({ id, ...alert }) =>
+          db
+            .insert(alertsTable)
+            .values({
+              alertId: id,
+              ...alert,
+              firstSeen: now,
+              lastSeen: now,
+            })
+            .onConflictDoUpdate({
+              target: alertsTable.alertId,
+              set: buildConflictUpdateColumns(alertsTable, ["id", "alertId", "firstSeen", "aiSummary"], true),
+            })
+        );
 
-        const updateCachedAlerts = async (): Promise<AlertsResponse> => {
-          const response = await fetch(`https://az511.com/api/v2/get/event?key=${env.AZ511_API_KEY}`);
-          const allAlerts = (await response.json()) as Alert[];
-          const alerts = allAlerts.filter((alert) => {
-            if (!alert.RoadwayName) return false;
-            const roadwayName = alert.RoadwayName.toUpperCase();
-            return /(SR|AZ?)[-\s]?347/gi.test(roadwayName);
-          });
+        await db.batch(operations as [(typeof operations)[0], ...typeof operations]);
 
-          const data = {
-            alerts,
-            lastUpdated: dayjs().toISOString(),
-          };
-
-          await redis.set("alerts", data);
-
-          return data;
-        };
-
-        if (value !== null) {
-          const expires = dayjs(value.lastUpdated).add(5, "minutes");
-
-          if (dayjs().isSameOrAfter(expires)) {
-            waitUntil(updateCachedAlerts());
-          }
-
-          return Response.json(value.alerts);
-        }
-
-        const { alerts } = await updateCachedAlerts();
-
-        return Response.json(alerts);
+        return Response.json({
+          success: true,
+          alerts,
+        });
       },
     },
   },
